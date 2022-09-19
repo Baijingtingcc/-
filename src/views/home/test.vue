2099b4695eb6c4b091c4622a6d7ad035b94a9425
lijiayin <1951433682@qq.com>
2022-09-19 09:29:55 +0800

    制造冲突

---
+++ src/views/home/test.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    制造冲突
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  components: {},
+  props: {},
+  data () {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created () {},
+  mounted () {},
+  methods: {}
+}
+</script>
+<style scoped lang="less"></style>
