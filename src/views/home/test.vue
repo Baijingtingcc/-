55d8044ca1c01157feb1f8f49e5eb00b7efa551c
lijiayin <1951433682@qq.com>
2022-09-19 09:37:56 +0800

    冲突2

---
+++ src/views/home/test.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    主分支冲突
+    冲突
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  components: {},
+  props: {},
+  data () {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created () {},
+  mounted () {},
+  methods: {}
+}
+</script>
+<style scoped lang="less"></style>
